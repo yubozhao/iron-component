620bb4a19876fbbf2fd08e67d438bc00c60609a6
Chris Mather <mather.chris@gmail.com>
2014-08-29 13:05:47 -0700

    fix to work with meteor 0.9.1-rc2

--- package.js
+++ package.js
@@ -1,21 +1,31 @@
 Package.describe({
   summary: 'UI Component base class',
-  version: "0.1.0"
+  version: "0.1.0",
+  git: "https://github.com/eventedmind/iron-component"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.1-rc2');
   api.use('underscore');
   api.use('jquery')
-  api.use('deps');
+  api.use('tracker');
   api.use('templating');
-  api.use('ui');
   api.use('blaze');
 
-  api.use('iron-core');
-  api.imply('iron-core');
+  api.use('iron:core@0.3.2');
+  api.imply('iron:core');
 
-  api.add_files('lib/component.js');
+  api.add_files('lib/component.js', 'client');
 });
 
 Package.on_test(function (api) {
+  api.use('iron:component');
+  api.use('templating');
+  api.use('tinytest');
+  api.use('test-helpers');
+  api.use('blaze');
+  api.use('tracker');
+
+  api.add_files('test/component_test.html', 'client');
+  api.add_files('test/component_test.js', 'client');
 });
